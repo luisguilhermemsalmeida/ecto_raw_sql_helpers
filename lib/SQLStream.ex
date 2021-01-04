224ec20003394879bc953e87a830c8ab236da21c
Luis Guilherme Almeida <luisguilhermemsalmeida@gmail.com>
2021-01-04 16:05:18 -0300

    Remove accidental require to IEx

--- lib/SQLStream.ex
+++ lib/SQLStream.ex
@@ -2,7 +2,6 @@ defmodule EctoRawSQLHelpers.SQLStream do
   alias EctoRawSQLHelpers.DatabaseResponseParser
   alias EctoRawSQLHelpers.AdapterSQLExecutor
   alias EctoRawSQLHelpers.StreamServer
-  require IEx
 
   def query(repo_or_pid, sql, params \\ [], options \\ []) do
     Stream.resource(
