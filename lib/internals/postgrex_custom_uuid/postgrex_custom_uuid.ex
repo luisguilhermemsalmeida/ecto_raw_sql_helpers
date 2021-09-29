402eee67e0b4c303d270e29561aec177254944a3
Luis Guilherme Almeida <luisguilhermemsalmeida@gmail.com>
2021-09-29 22:13:08 +0200

    Fix postgres dependency and publish hex

--- lib/internals/postgrex_custom_uuid/postgrex_custom_uuid.ex
+++ lib/internals/postgrex_custom_uuid/postgrex_custom_uuid.ex
@@ -1,22 +1,24 @@
-defmodule EctoRawSQLHelpers.Postgrex.CustomUUID do
-  import Postgrex.BinaryUtils, warn: false
-  use Postgrex.BinaryExtension, send: "uuid_send"
+if Code.ensure_loaded?(Postgrex) do
+  defmodule EctoRawSQLHelpers.Postgrex.CustomUUID do
+    import Postgrex.BinaryUtils, warn: false
+    use Postgrex.BinaryExtension, send: "uuid_send"
 
-  def encode(_) do
-    quote location: :keep do
-      uuid when is_binary(uuid) and byte_size(uuid) == 16 ->
-        [<<16::int32>> | uuid]
-      << a::64, 45::8, b::32, 45::8, c::32, 45::8, d::32, 45::8, e::96 >> ->
-        {:ok, uuid } = String.upcase(<< a::64, b::32, c::32, d::32, e::96 >>)
-        |> Base.decode16
-        [<<16 :: int32>> | uuid]
-      other ->
-        raise DBConnection.EncodeError, Postgrex.Utils.encode_msg(other, "a binary of 16 bytes or a valid UUID string representation")
+    def encode(_) do
+      quote location: :keep do
+        uuid when is_binary(uuid) and byte_size(uuid) == 16 ->
+          [<<16::int32>> | uuid]
+        << a::64, 45::8, b::32, 45::8, c::32, 45::8, d::32, 45::8, e::96 >> ->
+          {:ok, uuid } = String.upcase(<< a::64, b::32, c::32, d::32, e::96 >>)
+          |> Base.decode16
+          [<<16 :: int32>> | uuid]
+        other ->
+          raise DBConnection.EncodeError, Postgrex.Utils.encode_msg(other, "a binary of 16 bytes or a valid UUID string representation")
+      end
     end
-  end
-  def decode(_) do
-    quote location: :keep do
-      <<16::int32, uuid::binary-16>> -> Ecto.UUID.load(uuid) |> elem(1)
+    def decode(_) do
+      quote location: :keep do
+        <<16::int32, uuid::binary-16>> -> Ecto.UUID.load(uuid) |> elem(1)
+      end
     end
   end
 end
--- mix.exs
+++ mix.exs
@@ -11,6 +11,7 @@ defmodule EctoRawSQLHelpers.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       aliases: aliases(),
       package: package(),
+      description: description(),
     ]
   end
 
@@ -23,7 +24,8 @@ defmodule EctoRawSQLHelpers.MixProject do
 
   defp deps() do
     [
-      {:pipe_operators, git: "https://github.com/luisguilhermemsalmeida/pipe_operators.git", branch: "main"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:pipe_operators, "~> 0.1.1"},
       {:ecto_sql, ">= 2.0.0"},
       {:postgrex, ">= 0.0.0", only: [:test]},
       {:myxql, ">= 0.4.0", only: [:test]},
@@ -45,5 +47,14 @@ defmodule EctoRawSQLHelpers.MixProject do
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/luisguilhermemsalmeida/ecto_raw_sql_helpers"}
     ]
+  end
+
+  defp description() do
+    ~s"""
+    EctoRawSQLHelpers aims to improve raw SQL support in Ecto by providing helper functions, such as:
+    - Named parameters support
+    - Response parsing (getting query results as lists of maps, number of rows affected on statements, etc)
+    - Stream support for dealing with big result sets
+    """
   end
 end
